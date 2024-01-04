0dfe37fbe9df9e809f8543304b28dbe802cd9139
AfonsoCarvalhoo <145686711+AfonsoCarvalhoo@users.noreply.github.com>
2024-01-04 19:26:28 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,102 @@ Route::get('/sobre', function () {
 Route::get('/teste', function () {
     return view('teste');
 });
+
+
+
+
+
+**************************
+PARTE DE CARVALHO
+
+    
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\AuctionController;
+
+Route::post('/auctionlist', [AuctionController::class, 'auctiondados']);
+    
+
+Route::get('/', function () {
+    return view('1_PaginaInicial');
+});
+
+Route::get('/obras', function () {
+    return view('2_Obras');
+});
+
+Route::get('/licitar', function () {
+    return view('3_Licitar');
+});
+
+Route::get('/sobre', function () {
+    return view('4_SobreNos');
+});
+
+
+  
+Route::get('/teste', function () {
+    return view('teste');
+});
+
+Route::get('/formsauction', function () {
+    return view('6_FormsAuction');
+});
+
+
+
+
+Route::get('/CaravelasDetails', function () {
+    return view('CaravelasDetails');
+});
+
+Route::get('/JeanMichelSemTitulo', function () {
+    return view('Semtitulo1details');
+});
+
+Route::get('/DamaDouradaDetails', function () {
+    return view('DamaDouradaDetails');
+});
+
+Route::get('/JeanMichelSemTitulo2', function () {
+    return view('Semtitulo2details');
+});
+
+Route::get('/MarieDetails', function () {
+    return view('MarieDetails');
+});
+
+Route::get('/SalvadorDali', function () {
+    return view('SalvadorDaliDetails');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
